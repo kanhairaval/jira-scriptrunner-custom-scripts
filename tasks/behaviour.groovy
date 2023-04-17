d739d136cc714fd88e7bdb593f788c3757a05238
kanhairaval <kanhairaval@gmail.com>
2023-04-16 22:20:50 -0400

    fixed the issue with behaviour and now all 4 tasks/scripts work the way they are meant to work-KR

--- tasks/behaviour.groovy
+++ tasks/behaviour.groovy
@@ -3,31 +3,19 @@
 // priority field value is "Medium" or lower, remove the "URGENT: " summary prefix.
 // This Behaviour should not work on the Edit Screen.
 
-import com.atlassian.jira.component.ComponentAccessor
+import com.onresolve.jira.groovy.user.FieldBehaviours  // Importing FieldBehaviours from the package com.onresolve.jira.groovy.user
+import com.atlassian.jira.issue.priority.Priority      // Importing Priority from the package com.atlassian.jira.issue.priority
+import groovy.transform.BaseScript                     // Importing BaseScript from the package groovy.transform
 
-//Gets the Priority and Summary Fields
-def priorityField = getFieldById(getFieldChanged())
-def summaryField = getFieldById("summary")
+@BaseScript FieldBehaviours fieldBehaviours             // Setting the current script as a BaseScript for FieldBehaviours
 
-//Gets the value of the Priority Field
-def priorityValue = priorityField.getValue()
+def priorityField = getFieldById('priority')           // Getting the Priority field by its ID and storing it in the variable 'priorityField'
+def summaryField = getFieldById('summary')             // Getting the Summary field by its ID and storing it in the variable 'summaryField'
 
-//Checks if the Priority Field is the field that triggered the script
-if (getFieldChanged() == "priority") {
-    //Checks if the Priority Field is being set to High or Highest
-    if (priorityValue == "High" || priorityValue == "Highest") {
-        //Prepends "URGENT: " to the Summary Field
-        def summaryValue = "URGENT: " + summaryField.getValue()
-        summaryField.setFormValue(summaryValue)
-    }
-    //Checks if the Priority Field is being set to Medium or Lower
-    else if (priorityValue == "Medium" || priorityValue == "Low" || priorityValue == "Lowest") {
-        //Removes "URGENT: " from the Summary Field
-        def summaryValue = summaryField.getValue() as String
-        summaryValue = summaryValue.replace("URGENT: ", "")
-        summaryField.setFormValue(summaryValue)
-
-    }
+if (priorityField.value?.name in ['High', 'Highest']) { // Checking if the value of the Priority field is either 'High' or 'Highest'
+    summaryField.setFormValue('Urgent: ' + summaryField.value)  // Setting the form value of the Summary field to 'Urgent: ' + the current value of the Summary field
+} else {
+    summaryField.setFormValue(summaryField.value)      // Setting the form value of the Summary field to the current value of the Summary field
 }
 
 // The above code is a Behaviour which prefixes an issue's summary field to display "URGENT: " when the priority field value is "High" or "Highest". If the priority field value is "Medium" or lower, remove the "URGENT: " summary prefix. This Behaviour should not work on the Edit Screen.
